127f45cb97adcc6dc89a35b1693c1a6c065be9a6
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-03 20:38:14 +0000

    test fix

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@ export {
   serve,
   validateRequest,
 } from "https://deno.land/x/sift@0.3.0/mod.ts";
-export { verifySignature } from "https://github.com/discordeno/discordeno/raw/main/src/interactions/mod.ts";
+export * from "https://github.com/discordeno/discordeno/raw/main/src/interactions/mod.ts";
 // Interaction-related and utility types
 export * from "https://github.com/discordeno/discordeno/raw/main/src/types/mod.ts";
