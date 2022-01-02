9ab62aa64084334334ba41ec57e7ff933c2fde3a
ITOH <to@itoh.at>
2022-01-02 21:55:50 +0000

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,6 +3,6 @@ export {
   json,
   serve,
   validateRequest,
-} from "https://deno.land/x/sift@0.3.0/mod.ts";
+} from "https://deno.land/x/sift@0.4.2/mod.ts";
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts";
 export { decode } from "https://deno.land/std/encoding/base64url.ts"
